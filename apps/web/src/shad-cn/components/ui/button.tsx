c2ccfad86747746e41a7c39d3e7c189ef2b0ef79
sonwonjae <sonwj091552@gmail.com>
2024-12-27 20:51:06 +0900

    fix: button prop 수정

--- apps/web/src/shad-cn/components/ui/button.tsx
+++ apps/web/src/shad-cn/components/ui/button.tsx
@@ -34,7 +34,13 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: cn(
+          "text-slate-600",
+          "hover:text-primary",
+          "active:text-primary",
+          "underline-offset-4",
+          "hover:underline",
+        ),
       },
       size: {
         sm: "h-9 rounded-md px-4 py-2 text-sm",
