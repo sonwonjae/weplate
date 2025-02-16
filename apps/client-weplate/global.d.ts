a0fcc64f95c9a62945f37b9186bbf33e25d175b8
sonwonjae <sonwj091552@gmail.com>
2025-02-16 21:03:19 +0900

    fix: d.ts 파일에 global declare space 설정

--- apps/client-weplate/global.d.ts
+++ apps/client-weplate/global.d.ts
@@ -1,13 +1,14 @@
 import "@tanstack/react-query";
 
-type Nullable<T> = T | null;
+declare global {
+  type Nullable<T> = T | null;
 
-type UnionToIntersection<U> = (
-  U extends unknown ? (x: U) => void : never
-) extends (x: infer I) => void
-  ? I
-  : never;
-
+  type UnionToIntersection<U> = (
+    U extends unknown ? (x: U) => void : never
+  ) extends (x: infer I) => void
+    ? I
+    : never;
+}
 declare module "@tanstack/react-query" {
   interface Register {
     defaultError: AxiosError;
