7e43c84e1e16b5e94dd993ad7c3f89470fd17596
sonwonjae <sonwj091552@gmail.com>
2025-02-16 21:36:37 +0900

    feat: 음식 정보 등록 현황 새로고침 버튼 제공

--- apps/client-weplate/src/ui/member/Members/Title.tsx
+++ apps/client-weplate/src/ui/member/Members/Title.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { RotateCwIcon } from "lucide-react";
 import { useRouter } from "next/router";
 
 import { RQClient } from "@/utils/react-query";
@@ -10,20 +11,48 @@ function Title() {
   const assembleUserListQuery = new RQClient({
     url: `/api/assemble/${router.query.assembleId}/user/list`,
   });
-  const { data: assembleUserList = [] } = useQuery(
-    assembleUserListQuery.queryOptions,
-  );
+  const { data: assembleUserList = [], refetch: refetchAssembleUserList } =
+    useQuery(assembleUserListQuery.queryOptions);
 
   const registedMemberCount = assembleUserList.filter(({ isRegisted }) => {
     return isRegisted;
   }).length;
 
   return (
-    <h2 className={cn("pt-8", "px-5", "font-bold", "text-md")}>
-      음식 정보 등록 현황{" "}
+    <h2
+      className={cn(
+        "flex",
+        "items-center",
+        "pt-8",
+        "px-5",
+        "font-bold",
+        "text-md",
+        "gap-2",
+      )}
+    >
       <span>
+        <span>음식 정보 등록 현황 </span>
         <span className={cn("text-primary")}>{registedMemberCount}</span>/
         <span>{assembleUserList.length}</span>
+      </span>
+      <span
+        role="button"
+        className={cn(
+          "inline-block",
+          "p-1",
+          "cursor-pointer",
+          "rounded-full",
+          "bg-primary",
+        )}
+        onClick={() => {
+          refetchAssembleUserList();
+        }}
+      >
+        <RotateCwIcon
+          size={16}
+          strokeWidth={2.5}
+          className={cn("stroke-white")}
+        />
       </span>
     </h2>
   );
