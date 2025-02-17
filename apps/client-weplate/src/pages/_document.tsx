3dddc39a38cf9b23c79ea51026d320c4226e14c1
sonwonjae <sonwj091552@gmail.com>
2025-02-18 00:28:46 +0900

    style: body에 bg-white 적용

--- apps/client-weplate/src/pages/_document.tsx
+++ apps/client-weplate/src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+import { cn } from "@/utils/tailwind";
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className={cn("bg-white")}>
         <Main />
         <NextScript />
       </body>
